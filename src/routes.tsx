ec0990034d78dccf52ffd8fbd8c09d1f6e2a5c28
Mo-Wael <modywael195@gmail.com>
2026-08-24 19:07:37 +0300

    first commit in branch and deployment testing

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import Contact from "./Pages/Contact";
 import Education from "./Pages/Education";
 import Experiences from "./Pages/Experiences";
 import FirstPage from "./Pages/FirstPage";
+import NotFound from "./Pages/NotFound";
 import ProjectsPage from "./Pages/ProjectsPage";
 
 export const routers = [
@@ -9,5 +10,6 @@ export const routers = [
     {path: "/Education", component: Education},
     {path: "/Experiences", component: Experiences},
     {path: "/Projects", component: ProjectsPage},
-    {path: "/Contact", component: Contact}
+    {path: "/Contact", component: Contact},
+    {path: "*", component: NotFound}
 ]
